76e6c5e166f9b9c336cc9167bf5a3325df3c22a7
ny <na9173@suwon.ac.kr>
2024-01-09 15:45:45 +0900

    중복된 컴폰넌트 하나로 합침

--- src/main/java/com/kitri/myservletboard/dao/BoardJdbcDao.java
+++ src/main/java/com/kitri/myservletboard/dao/BoardJdbcDao.java
@@ -1,13 +1,13 @@
 package com.kitri.myservletboard.dao;
 
 import com.kitri.myservletboard.data.Board;
-import com.kitri.myservletboard.service.BoardService;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDateTime;
+import java.time.format.SignStyle;
 import java.util.ArrayList;
 
 public class BoardJdbcDao implements BoardDao{
@@ -88,21 +88,157 @@ public class BoardJdbcDao implements BoardDao{
 
     @Override
     public Board getById(Long id) {
-        return null;
+        // 상세 조회
+        // connection
+        // ps -> executeQuery();
+        // rs
+
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+
+        Board Board = new Board();
+        try {
+            connection = connectDB();
+            //상세 페이지를 구현하기 위해서는 id가 필요하기 때문에 id =? 로 id가져오기
+            // ? 나중에 값을 대입하겠다는 의미 => id가 계속 달라지기 때문에 (동적)
+            // 물음표 갯수대로 대체하는 것
+            String sql = "SELECT * FROM board WHERE id = ?";
+            ps = connection.prepareStatement(sql);
+            ps.setLong(1, id);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+//                id = rs.getLong("id");
+                String title = rs.getString("title");
+                String writer = rs.getString("writer");
+                String content = rs.getString("content");
+                LocalDateTime createdAt = rs.getTimestamp("created_at").toLocalDateTime();
+                int viewCount = rs.getInt("view_count");
+                int commentCount = rs.getInt("comment_count");
+
+                Board board_ = new Board(id, title, content, writer, createdAt, viewCount, commentCount);
+                return  board_;
+            }
+
+
+        } catch (Exception e) {
+        }
+        finally {
+            //무조건 실행
+            try {
+                rs.close();
+                ps.close();
+                connection.close();
+
+            }catch ( Exception e){
+                e.printStackTrace();
+            }
+        }
+        return Board;
     }
 
     @Override
     public void save(Board board) {
+        //게시물 등록하기 (글쓰기)
+        Connection connection = null;
+        PreparedStatement ps = null;
+
+
+        //폼에서 받은 제목, 작성자, 내용만 저장하고 다른 조회수나 댓글 수는 디비가 알아서 가져온다.
+
+        try {
+            connection = connectDB();
+            String insertsql = "INSERT INTO board (title, content, writer) VALUES (?,?,?)";
+//            rs = ps.executeQuery(insertsql);
+            ps = connection.prepareStatement(insertsql);
+
+                connection = connectDB();
+
+                ps.setString(1, board.getTitle());
+                ps.setString(2, board.getContent());
+                ps.setString(3, board.getWriter());
+                ps.executeUpdate();
+                // Update()문을 사용하면 입력받은 데이터를 갱신할 수 있다.
+
+
+        }catch (Exception e){
+
+        } finally {
+            try {
+                ps.close();
+                connection.close();
+
+            }catch ( Exception e){
+                e.printStackTrace();
+            }
+        }
 
     }
 
     @Override
     public void update(Board board) {
+        // 수정하기
+        Connection connection = null;
+        PreparedStatement ps = null;
+
+
+        //폼에서 받은 제목, 작성자, 내용만 저장하고 다른 조회수나 댓글 수는 디비가 알아서 가져온다.
+
+        try {
+            connection = connectDB();
+            String insertsql = "UPDATE board SET title = ?, content = ? WHERE id = ?";
+//            rs = ps.executeQuery(insertsql);
+            ps = connection.prepareStatement(insertsql);
+
+            connection = connectDB();
+
+            ps.setString(1, board.getTitle());
+            ps.setString(2, board.getContent());
+            ps.setLong(3, board.getId());
+            ps.executeUpdate();
+
+        }catch (Exception e){
+
+        } finally {
+            try {
+                ps.close();
+                connection.close();
+
+            }catch ( Exception e){
+                e.printStackTrace();
+            }
+        }
 
     }
 
+
     @Override
     public void delete(Board board) {
+        Connection connection = null;
+        PreparedStatement ps = null;
 
+        try {
+            connection = connectDB();
+
+            String query = "DELETE FROM board WHERE id = ?";
+
+            ps = connection.prepareStatement(query);
+
+            ps.setLong(1, board.getId());
+            ps.executeUpdate();
+
+        }catch (Exception e){
+
+        } finally {
+            try {
+                ps.close();
+                connection.close();
+
+            }catch ( Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ src/main/webapp/view/common/header.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kitri
+  Date: 2024-01-09
+  Time: 오후 4:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<header>
+    <a class="logo" href="/board/list"><span class="material-symbols-outlined">emoticon</span></a>
+    <nav>
+        <ul class="nav-items">
+            <li><a href="/board/list">게시글목록</a></li>
+            <li><a href="/board/createForm">게시글등록</a></li>
+            <li><a href="/board/updateForm">게시글수정</a></li>
+            <li><a href="/view/member/join.jsp">회원가입</a></li>
+            <li><a href="/view/member/registration.jsp">회원정보수정</a></li>
+            <li><a href="/view/member/login.jsp">로그인</a></li>
+        </ul>
+    </nav>
+</header>
+
