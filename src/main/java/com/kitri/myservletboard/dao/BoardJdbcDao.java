10181d992d268f7c29bcbebc1016ac5791a786ac
ny <na9173@suwon.ac.kr>
2024-01-11 09:31:36 +0900

    BoardJdbcDao 구현 전

--- src/main/java/com/kitri/myservletboard/dao/BoardJdbcDao.java
+++ src/main/java/com/kitri/myservletboard/dao/BoardJdbcDao.java
@@ -1,2 +1,32 @@
-package com.kitri.myservletboard.dao;public class BoardJdbcDao {
+package com.kitri.myservletboard.dao;
+
+import com.kitri.myservletboard.data.Board;
+
+import java.util.ArrayList;
+
+public class BoardJdbcDao implements BoardDao {
+    @Override
+    public ArrayList<Board> getAll() {
+        return null;
+    }
+
+    @Override
+    public Board getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(Board board) {
+
+    }
+
+    @Override
+    public void update(Board board) {
+
+    }
+
+    @Override
+    public void delete(Board board) {
+
+    }
 }
